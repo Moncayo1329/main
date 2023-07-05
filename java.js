251fbbab1a51dea359bb1dd6bd9052f43bb9e24f
Moncayo1329 <86531347+Moncayo1329@users.noreply.github.com>
2023-07-05 14:50:48 +0100

    add

--- java.js
+++
@@ -1,7 +0,0 @@
-// JavaScript
-const menuContainer = document.getElementById('menu-container');
-const menu = document.getElementById('menu');
-
-menuContainer.addEventListener('click', () => {
-  menu.style.display = menu.style.display === 'block' ? 'none' : 'block';
-});
